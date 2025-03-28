f7d542e89b91add3867f18511318098e47d6ef74
mirabeylice <mira.beylicee0@gmail.com>
2025-03-28 17:31:39 +0300

    fix: firebase.js secret path düzeltildi

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 
 // Çevresel değişkenden (env) firebase anahtarını alıyoruz
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
+const firebaseKey = require("/etc/secrets/firebaseKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
