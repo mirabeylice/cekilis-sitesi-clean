62a8131e52293e668d079d48c3984d45519c6e77
mirabeylice <mira.beylicee0@gmail.com>
2025-04-05 21:55:20 +0300

    Firebase bağlantısı için environment desteği eklendi

--- firebase.js
+++ firebase.js
@@ -1,18 +1,22 @@
-require("dotenv").config();
-const admin = require("firebase-admin");
+require('dotenv').config();
+const admin = require('firebase-admin');
 
-let firebaseKey;
-
-try {
-  firebaseKey = JSON.parse(process.env.FIREBASE_KEY_JSON);
-} catch (err) {
-  console.error("FIREBASE_KEY_JSON env değişkeni okunamadı veya parse edilemedi.");
-  process.exit(1);
-}
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseKey),
-  databaseURL: "https://cekilis-sitesi-default-rtdb.europe-west1.firebasedatabase.app"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://cekilis-sitesi-default-rtdb.europe-west1.firebasedatabase.app' // burası senin URL'in
 });
 
 const db = admin.database();
