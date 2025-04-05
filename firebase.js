e00b56c6b4cfddf8caddc359cd1535572eb5cebf
mirabeylice <mira.beylicee0@gmail.com>
2025-04-05 20:20:48 +0300

    Firebase key dosya yerine env'den alınacak şekilde güncellendi

--- firebase.js
+++ firebase.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("/etc/secrets/firebaseKey.json"); // Render'da Secret File olarak yüklenecek
+
+let firebaseKey;
+
+try {
+  firebaseKey = JSON.parse(process.env.FIREBASE_KEY_JSON);
+} catch (err) {
+  console.error("FIREBASE_KEY_JSON env değişkeni okunamadı veya parse edilemedi.");
+  process.exit(1);
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseKey),
   databaseURL: "https://cekilis-sitesi-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
 const db = admin.database();
+
 module.exports = { db };
